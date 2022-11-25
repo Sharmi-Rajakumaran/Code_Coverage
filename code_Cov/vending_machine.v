873a19c7f5b2fcf81ba47beaa308eacd34a91a19
SHARMI RAJAKUMARAN <106381150+Sharmi-Rajakumaran@users.noreply.github.com>
2022-11-25 21:48:50 +0530

    Update vending_machine.v

--- code_Cov/vending_machine.v
+++ code_Cov/vending_machine.v
@@ -87,5 +87,70 @@ module coincol (clock,reset,coin_in,done_out,lsb7seg_out,msb7seg_out);
    assign done_out = (prestate == STATE100) ? 1'b1 : 1'b0;
 
    /************************************************
+   
+    //Output logic
+   assign done_out = (prestate == STATE100) ? 1'b1 : 1'b0;
+
+   /************************************************
+
+   SEVEN SEGMENT LED DISPLAY
+
+       a
+      ---    
+   f |   | b
+      ---  <-- g
+   e |   | c
+      ---
+      d  
+
+   Each LED segment is active low.
+   Both lsb7seg and msb7seg are assigned
+   in the order "gfedcba"
+
+   ************************************************/
+
+   always @(prestate)
+      begin
+         case(prestate)
+            STATE25  :
+               
+                        begin
+                           lsb7seg_out = 7'b0100100;
+                           msb7seg_out = 7'b0010100;
+                        end
+            
+            STATE50  :
+               
+                        begin
+                           lsb7seg_out = 7'b0100010;
+                           msb7seg_out = 7'b1000000;
+                        end
+            
+            STATE75  :
+               
+                        begin
+                           lsb7seg_out = 7'b1111000;
+                           msb7seg_out = 7'b0010010;
+                       end
+            
+            STATE100 :
+               
+                        begin
+                           lsb7seg_out = 7'b0001001;
+                           msb7seg_out = 7'b0001000;
+                        end
+            
+            default  :
+                        begin
+                           lsb7seg_out = 7'b1000000;
+                           msb7seg_out = 7'b1000000;
+                        end
+            
+         endcase
+          end
+
+
+
+endmodule
 
 
