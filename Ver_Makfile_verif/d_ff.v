68874feed9e3eb09f3991402fe04a781fe942469
SHARMI RAJAKUMARAN <106381150+Sharmi-Rajakumaran@users.noreply.github.com>
2022-11-25 09:26:20 +0530

    Create d_ff.v

---
+++ Ver_Makfile_verif/d_ff.v
@@ -0,0 +1,28 @@
+module d_ff(input d0, d1, sel, clk, rst, q);
+  input d0,d1,sel,clk,rst;
+  output reg q;
+  reg d;
+  
+  always@(d0 or d1 or sel)
+    begin
+      case (sel)
+          0 : d = d0;
+          1 : d = d1;
+          default: d = 1'b0;
+    end
+  
+  always@(posedge clk or posedge rst)
+    begin
+    begin
+      if(rst)
+        q<=1'b0;
+      else
+      q <= d;
+    end
+        assign qbar = ~q;
+        
+     end
+        
+   endmodule
+   
+  
