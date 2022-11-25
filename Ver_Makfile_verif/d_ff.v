f1cd8eaf8e03a792598d7ffaca4b8531140c362b
SHARMI RAJAKUMARAN <106381150+Sharmi-Rajakumaran@users.noreply.github.com>
2022-11-25 12:44:59 +0530

    Update d_ff.v

--- Ver_Makfile_verif/d_ff.v
+++ Ver_Makfile_verif/d_ff.v
@@ -25,5 +25,27 @@ module d_ff(input d0, d1, sel, clk, rst,output reg q);
     
         
    endmodule
+
+/*
+Simulation Result
+# Loading work.tb_dff(fast)
+# run -all
+# Reset is perfect
+# Input is perfect
+# Reset is perfect
+# Input is perfect
+# Input is perfect
+# Input is perfect
+# Input is perfect
+# Reset is perfect
+# ** Note: $stop    : ../tb/tb_dff.v(118)
+#    Time: 995 ns  Iteration: 0  Instance: /tb_dff
+# Break at ../tb/tb_dff.v line 118
+# Stopped at ../tb/tb_dff.v line 118
+#  exit
+# End time: 12:44:11 on Nov 25,2022, Elapsed time: 0:00:00
+# Errors: 0, Warnings: 0
+
+*/
    
   
